f4db9b5c56e5f28858631be8f41ee2e9cbaa02f4
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2020-12-11 16:27:37 -0500

    Update process.php

--- process.php
+++ process.php
@@ -12,7 +12,28 @@
     $fields{"phone"} = "phone";
     $fields{"message"} = "message";
 
+    $recaptcha = $_POST['g-recaptcha-response'];
+    $res = reCaptcha($recaptcha);
+    if(!$res['success']){
+      // Error
+    }
 
+    function reCaptcha($recaptcha){
+      $secret = "6LervAIaAAAAAHCfbGl_6AA4ap7RvwGSUSsOMPqb";
+      $ip = $_SERVER['REMOTE_ADDR'];
+
+      $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+      $url = "https://www.google.com/recaptcha/api/siteverify";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+      $data = curl_exec($ch);
+      curl_close($ch);
+
+      return json_decode($data, true);
+    }
 
     $body = "Here is what was sent:\r\n";
 
